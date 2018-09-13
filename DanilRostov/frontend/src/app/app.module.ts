519119064602b65ce4b7f38a5e30493d0baa3cf7
Danil <daniil.rossy@gmail.com>
2018-09-13 12:07:10 +0300

    implemented router

--- DanilRostov/frontend/src/app/app.module.ts
+++ DanilRostov/frontend/src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 import { PokemonsListComponent } from './components/pokemons-list/pokemons-list.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { HttpModule } from '@angular/http';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { CatchedPageComponent } from './components/catched-page/catched-page.component';
+import { RouterModule } from '@angular/router';
+// import { routes } from './routes';
+
+const routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'catched',
+    component: CatchedPageComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +28,13 @@ import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.com
     PokemonsListComponent,
     NavigationComponent,
     HomePageComponent,
-    PokemonCardComponent
+    PokemonCardComponent,
+    CatchedPageComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ DanilRostov/frontend/src/app/components/catched-page/catched-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catched-page',
+  templateUrl: './catched-page.component.html',
+  styleUrls: ['./catched-page.component.css']
+})
+export class CatchedPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
