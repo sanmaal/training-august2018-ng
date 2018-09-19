941e6b2ff9a23a5c210b6ee3b5fbc47df4ca7ddc
Alena Ianova <alena_ianova@epam.com>
2018-09-19 19:25:08 +0300

    add error service; add common function for login and register

--- AlenaIanova/Pokedex/src/app/auth/shared/auth.service.ts
+++ AlenaIanova/Pokedex/src/app/auth/shared/auth.service.ts
@@ -3,8 +3,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SessionService } from '../../shared/session.service';
 import { User, NewUser } from './user';
 import { environment } from '../../../environments/environment';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { ErrorService } from '../../shared/error.service';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -17,36 +18,25 @@ export class AuthService {
   constructor(
     private http: HttpClient,
     private sessionService: SessionService,
+    private errorService: ErrorService
   ) { }
 
-  register(newUser: NewUser): Observable<any> {
-    return this.http.post<any>(`${environment.host}register`, newUser, httpOptions)
+  private authQuery (queryName: string, body: object) {
+    return this.http.post<any>(`${environment.host}${queryName}`, body, httpOptions)
       .pipe(
         tap(res => {
           this.sessionService.logIn(res.token);
         }),
-        catchError(this.handleError<any>())
+        catchError(this.errorService.handleError())
       );
+  }
+
+  register(newUser: NewUser): Observable<any> {
+    return this.authQuery('register', newUser);
   }
 
   login(user: User): Observable<any> {
-    return this.http.post<any>(`${environment.host}login`, user, httpOptions)
-      .pipe(
-        tap(res => {
-          this.sessionService.logIn(res.token);
-        }),
-        catchError(this.handleError<any>())
-      );
-  }
-
-  private handleError<T> () {
-    return (error: any): Observable<any> => {
-      if (error.error.error) {
-        return of(error.error);
-      } else {
-        return of({error: 'Something wrong was happen. Please try again.'});
-      }
-    };
+    return this.authQuery('login', user);
   }
 
 }
--- AlenaIanova/Pokedex/src/app/pokemons/shared/pokemons.service.ts
+++ AlenaIanova/Pokedex/src/app/pokemons/shared/pokemons.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { PokemonsList } from './pokemons-list';
 import { Pokemon } from './pokemon';
 import { environment } from '../../../environments/environment';
+import { ErrorService } from '../../shared/error.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -17,6 +18,7 @@ export class PokemonsService {
 
   constructor(
     private http: HttpClient,
+    private errorService: ErrorService
   ) {}
 
   private pokemonsUrl = `${environment.host}pokemons/`;
@@ -24,37 +26,28 @@ export class PokemonsService {
   getPokemons(page: number): Observable<PokemonsList> {
     return this.http.get<PokemonsList>(`${this.pokemonsUrl}page/${page}`, httpOptions)
       .pipe(
-        catchError(this.handleError<PokemonsList>())
+        catchError(this.errorService.handleError<PokemonsList>())
       );
   }
 
   getCaughtPokemons(page: number): Observable<PokemonsList> {
     return this.http.get<PokemonsList>(`${this.pokemonsUrl}caught/page/${page}`, httpOptions)
       .pipe(
-        catchError(this.handleError<PokemonsList>())
+        catchError(this.errorService.handleError<PokemonsList>())
       );
   }
 
   getPokemon(id: number | string): Observable<Pokemon> {
     return this.http.get<Pokemon>(`${this.pokemonsUrl}${id}`)
       .pipe(
-        catchError(this.handleError<Pokemon>())
+        catchError(this.errorService.handleError<Pokemon>())
       );
   }
 
   catchPokemon(id: number): Observable<Pokemon> {
     return this.http.put<Pokemon>(`${this.pokemonsUrl}${id}/catch`, httpOptions)
       .pipe(
-        catchError(this.handleError<Pokemon>())
+        catchError(this.errorService.handleError<Pokemon>())
       );
-  }
-
-  private handleError<T> () {
-    return (error: any): Observable<any> => {
-      if (error.error.error) {
-        return of(error.error);
-      }
-      return of({error: 'Something wrong was happen'});
-    };
   }
 }
